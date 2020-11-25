ea310f490da9bc1d1bbe880ff6272df3447dd245
Attila L. Egyedi <attila.egyedi@gmail.com>
2020-11-25 13:48:16 -0800

    Name change

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/cedar-cee-demo'),
+      dir: require('path').join(__dirname, './coverage/cedar-cee-demo-angular'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cedar-cee-demo'`, () => {
+  it(`should have as title 'cedar-cee-demo-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cedar-cee-demo');
+    expect(app.title).toEqual('cedar-cee-demo-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('cedar-cee-demo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('cedar-cee-demo-angular app is running!');
   });
 });
